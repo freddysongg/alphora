1763fb0e477361d459a474e3f3afbf4ca4aa24f3
Freddy Song <fredsong99@gmail.com>
2026-05-18 17:25:35 -0700

    expose 8 new source-client public apis from package root

--- services/api/app/services/source_clients/__init__.py
+++ services/api/app/services/source_clients/__init__.py
@@ -5,10 +5,55 @@ from app.services.source_clients._http import (
     SourceClientRateLimitError,
     SourceClientTimeoutError,
 )
+from app.services.source_clients.ainvest import (
+    AinvestCongressTransaction,
+    AinvestCongressTransactionsResponse,
+    fetch_ainvest_congress_transactions,
+)
+from app.services.source_clients.congress_gov import (
+    CongressBill,
+    CongressBillsResponse,
+    CongressMember,
+    CongressMembersResponse,
+    fetch_congress_bills,
+    fetch_congress_members,
+)
 from app.services.source_clients.fred import (
     FredObservation,
     FredSeriesObservations,
     fetch_series_observations,
+)
+from app.services.source_clients.gleif import (
+    GleifLeiRecord,
+    GleifSearchResponse,
+    fetch_gleif_by_lei,
+    fetch_gleif_search,
+)
+from app.services.source_clients.kalshi import (
+    KalshiMarket,
+    KalshiMarketDetailResponse,
+    KalshiMarketsResponse,
+    fetch_kalshi_market_detail,
+    fetch_kalshi_markets,
+)
+from app.services.source_clients.openfigi import (
+    OpenFigiMappingResponse,
+    OpenFigiResult,
+    fetch_openfigi_mapping,
+)
+from app.services.source_clients.polygon import (
+    PolygonAggregateBar,
+    PolygonAggregatesResponse,
+    PolygonTicker,
+    PolygonTickersResponse,
+    fetch_polygon_aggregates,
+    fetch_polygon_tickers,
+)
+from app.services.source_clients.polymarket import (
+    PolymarketEvent,
+    PolymarketMarket,
+    fetch_polymarket_events,
+    fetch_polymarket_markets,
 )
 from app.services.source_clients.sec_edgar import (
     SecCompanyTicker,
@@ -18,10 +63,35 @@ from app.services.source_clients.sec_edgar import (
     fetch_company_tickers,
     fetch_submissions,
 )
+from app.services.source_clients.tiingo import (
+    TiingoDailyPriceRow,
+    TiingoIexQuote,
+    fetch_tiingo_daily_prices,
+    fetch_tiingo_latest,
+)
 
 __all__ = [
+    "AinvestCongressTransaction",
+    "AinvestCongressTransactionsResponse",
+    "CongressBill",
+    "CongressBillsResponse",
+    "CongressMember",
+    "CongressMembersResponse",
     "FredObservation",
     "FredSeriesObservations",
+    "GleifLeiRecord",
+    "GleifSearchResponse",
+    "KalshiMarket",
+    "KalshiMarketDetailResponse",
+    "KalshiMarketsResponse",
+    "OpenFigiMappingResponse",
+    "OpenFigiResult",
+    "PolygonAggregateBar",
+    "PolygonAggregatesResponse",
+    "PolygonTicker",
+    "PolygonTickersResponse",
+    "PolymarketEvent",
+    "PolymarketMarket",
     "SecCompanyTicker",
     "SecCompanyTickersResponse",
     "SecRecentSubmission",
@@ -31,7 +101,23 @@ __all__ = [
     "SourceClientHTTPError",
     "SourceClientRateLimitError",
     "SourceClientTimeoutError",
+    "TiingoDailyPriceRow",
+    "TiingoIexQuote",
+    "fetch_ainvest_congress_transactions",
     "fetch_company_tickers",
+    "fetch_congress_bills",
+    "fetch_congress_members",
+    "fetch_gleif_by_lei",
+    "fetch_gleif_search",
+    "fetch_kalshi_market_detail",
+    "fetch_kalshi_markets",
+    "fetch_openfigi_mapping",
+    "fetch_polygon_aggregates",
+    "fetch_polygon_tickers",
+    "fetch_polymarket_events",
+    "fetch_polymarket_markets",
     "fetch_series_observations",
     "fetch_submissions",
+    "fetch_tiingo_daily_prices",
+    "fetch_tiingo_latest",
 ]
